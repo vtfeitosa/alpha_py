e33928240dfedca34aa33d6ee2a5e87aa40812ff
vtfeitosa <vfmidia@hotmail.com>
2023-01-24 15:54:00 -0300

    add ex.002-3

---
+++ Python/Aula_02/tipo_letra.py
@@ -0,0 +1,24 @@
+# Vogais e Consoantes
+
+L = input("Digite uma letra: ")
+
+if L.upper() == "A":
+    tipo = "Vogal"
+
+elif L.upper() == "E":
+    tipo = "Vogal"
+
+elif L.upper() == "I":
+    tipo = "Vogal"
+
+elif L.upper() == "O":
+    tipo = "Vogal"
+
+elif L.upper() == "U":
+    tipo = "Vogal"
+
+else:
+    tipo = "Consoante"
+
+print("A letra é ", tipo)
+
