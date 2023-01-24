a345ab10f2fbd1b6623f6dfdf3abc649142b7eae
vtfeitosa <vfmidia@hotmail.com>
2023-01-24 15:29:53 -0300

    add ex.001-3

---
+++ Python/Aula_01/temperatura/001-3.py
@@ -0,0 +1,9 @@
+#Conversor de Temperatura C >> F >> K
+
+C = float(input("Temperaturas em Graus Celsius: "))
+
+K = C + 273.15
+F = (C * 1.8 ) + 32
+
+print("Fahrenheit: ", F )
+print("Kelvin: ", K)
